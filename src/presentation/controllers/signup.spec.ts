c8fcf80107171f0fbe6451aeeaf57d163158667a
guilherme-souza-lima <guilherme_souza.lima@hotmail.com>
2023-02-05 19:19:49 -0300

    test: ensure SignUpController return 400 if no password provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -17,7 +17,7 @@ describe('SignUp Controller', () => {
     expect(httpResponse.body).toEqual(new MissingParamError('name'))
   })
 
-  test('Shoulf return 400 if no name is provided', () => {
+  test('Shoulf return 400 if no email is provided', () => {
     const sut = new SignUpController()
     const httpRequest = {
       body: {
@@ -30,5 +30,20 @@ describe('SignUp Controller', () => {
     const httpResponse = sut.handler(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('email'))
+  })
+
+  test('Shoulf return 400 if no password is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        // password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handler(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('password'))
   })
 })
